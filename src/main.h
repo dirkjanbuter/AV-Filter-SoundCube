62f85b516f249f8290cb78a18b9541a2d8cb908f
dirkjan <mailbox@dirkjanbuter.com>
2024-12-09 11:11:57 +0100

    first

---
+++ src/main.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+
+
+#include "vector2d.h"
+#include "vector3d.h"
+#include "matrix3d.h"
+
+extern int filtercreate(int fps);
+extern int filterdestroy();
+extern int filterstep(unsigned char *buffer, int w, int h, unsigned int color, char *text, int64_t framecount);
+extern int filteraudio(float *frame, int64_t framecount, float elapsed);
+extern int filtermic(float *frame, int64_t framecount, float elapsed);
